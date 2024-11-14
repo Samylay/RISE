5718a6daa889c959f09cb118818eb8e7be1c76fd
Samy LAYAIDA <samy.layaida@epita.fr>
2024-11-14 14:49:56 +0100

    tech: PWA

--- application/app/layout.tsx
+++ application/app/layout.tsx
@@ -13,11 +13,49 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+
+const APP_NAME = "Rise";
+const APP_DEFAULT_TITLE = "Rise";
+const APP_TITLE_TEMPLATE = "%s - Rise";
+const APP_DESCRIPTION =
+  "RISE transforms personal development into an engaging journey where every action contributes to measurable growth.";
+
 export const metadata: Metadata = {
-  title: "Rise",
-  description:
-    "RISE transforms personal development into an engaging journey where every action contributes to measurable growth.",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
+
 
 export default function RootLayout({
   children,
